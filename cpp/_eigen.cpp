ef25ec1a839cd72034405c9a0cad65dca3886207
SUNGOD3 <sun1223god@gmail.com>
2023-12-14 07:00:28 +0000

    threads 1

--- cpp/_eigen.cpp
+++ cpp/_eigen.cpp
@@ -1,5 +1,5 @@
 #include "_eigen.hpp"
-#define thread_num 4
+#define thread_num 1
 std::pair<Matrix, Matrix> QR_decomposition_wrapper(Matrix const& mat, int type)
 {
     if (type == 1) {
--- cpp/_matrix.hpp
+++ cpp/_matrix.hpp
@@ -1,6 +1,6 @@
 #ifndef __MATRIX_HPP__
 #define __MATRIX_HPP__
-#define thread_num 4
+#define thread_num 1
 constexpr double EPSILON = 0.000001;
 
 #include <iostream>
